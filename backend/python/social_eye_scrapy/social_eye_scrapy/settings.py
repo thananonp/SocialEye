b37bb755a1f76b137a00a981e692214972b79188
Thananon Pongsuwan <thenightraven7407@gmail.com>
2020-10-22 16:40:10 +0700

    Add method for get data inside webpage

--- backend/python/social_eye_scrapy/social_eye_scrapy/settings.py
+++ backend/python/social_eye_scrapy/social_eye_scrapy/settings.py
@@ -17,7 +17,7 @@ NEWSPIDER_MODULE = 'social_eye_scrapy.spiders'
 USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
--- backend/python/social_eye_scrapy/social_eye_scrapy/spiders/teedin108_spider.py
+++ backend/python/social_eye_scrapy/social_eye_scrapy/spiders/teedin108_spider.py
@@ -1,4 +1,6 @@
 import scrapy
+import re
+import string
 
 
 class QuotesSpider(scrapy.Spider):
@@ -7,10 +9,12 @@ class QuotesSpider(scrapy.Spider):
     def start_requests(self):
         urls = [
             # 'https://www.teedin108.com/house/amphur/43/'
-            './htmlsample/teedin108_inside.html'
+            # 'D:/SocialEye/backend/python/social_eye_scrapy/social_eye_scrapy/spiders/htmlsample/teedin108_inside.html'
+            # 'https://www.teedin108.com/land/view/2237655/'
+            'https://www.teedin108.com/land/view/2238699/'
         ]
         for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+            yield scrapy.Request(url=url, callback=self.parseinside)
 
     # def parse(self, response):
     #     page = response.url.split("/")[-2]
@@ -19,29 +23,40 @@ class QuotesSpider(scrapy.Spider):
     #         f.write(response.body)
     #     self.log('Saved file %s' % filename)
 
-    def parse(self, response):
-        for container in response.css('div.container'):
-            for row_a in container.css('div.row'):
-                for colsm9 in row_a.css('div.col-sm-9'):
-                    for row_b in colsm9.css('div.row'):
-                        for colxs12 in row_b.css('div.col-xs-12'):
-                            for row_c in colxs12.css('div.row'):
-                                yield {
-                                    'name': row_c.css('div.col-xs-12 a::text').get(),
-                                    'price': row_c.css('div.priceinlist::text')[1].get(),
-                                    'link': row_c.css('div.col-xs-12 a::attr(href)').get()
-                                }
-                                next_page = row_c.css(
-                                    'div.col-xs-12 a::attr(href)').get()
-                                print("following " + next_page)
-                                if next_page is not None:
-                                    yield response.follow(next_page, callback=self.parseinside)
-
     def parseinside(self, response):
+        # for info in response.css('div.working-area div.row div.padding-right-10 div.row article'):
         for info in response.css('div.working-area div.row div.padding-right-10 div.row article'):
             yield {
-                'info': info.css('div.poster-detail::text').getall()
+                'info': re.sub('\s{2,}', '', "".join(info.css('div.description::text').getall())),
+                'name': re.sub('\s{2,}', '', info.css('div.poster-detail::text').getall()[1]),
+                'line': re.sub('\s{2,}', '', info.css('div.poster-detail::text').getall()[2]),
+                'telephone': info.css('div.poster-detail a::text').getall()[0],
+                'email': info.css('div.poster-detail a::text').getall()[1]
             }
+
+    # def parse(self, response):
+    #     for container in response.css('div.container'):
+    #         for row_a in container.css('div.row'):
+    #             for colsm9 in row_a.css('div.col-sm-9'):
+    #                 for row_b in colsm9.css('div.row'):
+    #                     for colxs12 in row_b.css('div.col-xs-12'):
+    #                         for row_c in colxs12.css('div.row'):
+    #                             yield {
+    #                                 'name': row_c.css('div.col-xs-12 a::text').get(),
+    #                                 'price': row_c.css('div.priceinlist::text')[1].get(),
+    #                                 'link': row_c.css('div.col-xs-12 a::attr(href)').get()
+    #                             }
+    #                             next_page = row_c.css(
+    #                                 'div.col-xs-12 a::attr(href)').get()
+    #                             print("following " + next_page)
+    #                             if next_page is not None:
+    #                                 yield response.follow(next_page, callback=self.parseinside)
+
+    # def parseinside(self, response):
+    #     for info in response.css('div.working-area div.row div.padding-right-10 div.row article'):
+    #         yield {
+    #             'info': info.css('div.poster-detail::text').getall()
+    #         }
 
     # container = response.css('div.container')
     # row1 = container.css('div.row')
