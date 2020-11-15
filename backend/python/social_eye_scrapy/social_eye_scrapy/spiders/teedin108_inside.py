efa74426dc829d07c92326acd97d84c5ec84763a
Thananon Pongsuwan <thenightraven7407@gmail.com>
2020-11-15 13:05:25 +0700

    seperate crawling to mainpage and inside

---
+++ backend/python/social_eye_scrapy/social_eye_scrapy/spiders/teedin108_inside.py
@@ -0,0 +1,42 @@
+import json
+import scrapy
+import re
+# urls = []
+# f = open('./result/teedin108_23.json', encoding='utf8')
+# data = json.load(f)
+# for i in data:
+#     # print(i['link'])
+
+#     urls.append(i['link'])
+# f.close()
+# print(urls)
+
+
+class QuotesSpider(scrapy.Spider):
+    name = "teedin108inside"
+
+    def start_requests(self):
+        urls = []
+        f = open('./result/teedin108_23.json', encoding='utf8')
+        data = json.load(f)
+        for i in data:
+            # print(i['link'])
+            urls.append(i['link'])
+        f.close()
+
+        # tag = getattr(self, 'tag', None)
+        # if tag is not None:
+        #     url = url + tag
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parseinside)
+
+    def parseinside(self, response):
+        # for info in response.css('div.working-area div.row div.padding-right-10 div.row article'):
+        for info in response.css('div.working-area div.row div.padding-right-10 div.row article'):
+            yield {
+                'info': re.sub('\s{2,}', '', "".join(info.css('div.description::text').getall())),
+                'name': re.sub('\s{2,}', '', info.css('div.poster-detail::text').getall()[1]),
+                'line': re.sub('\s{2,}', '', info.css('div.poster-detail::text').getall()[2]),
+                'telephone': info.css('div.poster-detail a::text').getall()[0],
+                'email': info.css('div.poster-detail a::text').getall()[1]
+            }
