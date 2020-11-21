2c7356235def09d40250dfdd1fdf8985a2c1f014
Thananon Pongsuwan <thenightraven7407@gmail.com>
2020-11-21 10:51:55 +0700

    Update DDPropertyRSSCondo.py to have reading and writing

--- backend/python/social_eye_scrapy/social_eye_scrapy/spiders/DDPropertyRSSCondo.py
+++ backend/python/social_eye_scrapy/social_eye_scrapy/spiders/DDPropertyRSSCondo.py
@@ -1,13 +1,29 @@
 import json
 import re
+from datetime import datetime
 import feedparser
 
 url = "https://www.ddproperty.com/rss/sale/N"
+filename = './result/ddProperty/condo.json'
+
+
+def write_json(data, filename=filename):
+    print("Added Item " + str(added_element))
+    with open(filename, 'w', encoding='utf-8') as outfile:
+        json.dump(data, outfile, ensure_ascii=False)
+
+
 NewsFeed = feedparser.parse(url)
+added_element = 0
 entry = NewsFeed.entries[1]
 # print(NewsFeed.entries[0])
 print(entry.keys())
 data = {'ddProperty': []}
+with open(filename, mode='r', encoding='utf-8') as f:
+    data_old = json.load(f)
+    temp = data['ddProperty']
+    # for item in data_old['ddProperty']:
+    #     data['ddProperty'].append(item)
 
 for item in NewsFeed.entries:
     title = item.title
@@ -32,17 +48,15 @@ for item in NewsFeed.entries:
     # print("price " + price)
     # print("sizeInMeter " + sizeInMeter)
     # print("link " + link)
+    if link not in data_old['ddProperty']:
+        temp.append({
+            'title': title,
+            'category': category,
+            'pubDate': pubDate,
+            'price': price,
+            'size': sizeInMeter,
+            'link': link
+        })
+        ++added_element
 
-    data['ddProperty'].append({
-        'title': title,
-        'category': category,
-        'pubDate': pubDate,
-        'price': price,
-        'size': sizeInMeter,
-        'link': link
-    })
-
-with open('./result/ddPropertyCondo.json', 'w', encoding='utf-8') as outfile:
-    json.dump(data, outfile, ensure_ascii=False)
-
-# todo change to folder and merge.
+write_json(data)
